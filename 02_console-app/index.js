e0bd7756bd5365f623ee1f9340cda5319d90b86f
olekskhom <oleks.khom@gmail.com>
2023-12-05 14:00:55 +0300

    argv_process

---
+++ 02_console-app/index.js
@@ -0,0 +1,20 @@
+// console.log(global)
+// process.env.HELLO = 'HELLO';
+// console.log(process)
+
+
+// шлях до ноди
+// шлях до скрипта
+// console.log(process.argv)
+
+
+// // поточна дерикторія
+// console.log(process.cwd())
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(1111111)
+// // зупиняє скрипт
+// process.exit();
+// console.log(22222)
+
